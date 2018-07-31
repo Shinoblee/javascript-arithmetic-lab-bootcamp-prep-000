7a13278c878cfcfd3adc091edb941d2618f0b576
Lee Simon <shinoblee@gmail.com>
2018-07-31 13:38:09 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,33 @@
+require('test/index-test.js');
+
+function add() {
+  return a + b;
+}
+
+function subtract() {
+  return a - b;
+}
+
+function multiply() {
+  return a * b;
+}
+
+function divide() {
+  return a / b;
+}
+
+function inc(n) {
+  return a++;
+}
+
+function dec(n) {
+  return a--;
+}
+
+function makeInt(n) {
+  return parseInt(a);
+}
+
+function preserveDecimal(n) {
+  return parseFloat(a);
+}
